effcc96ea138279530cec324be05452419817062
xdvt1337 <76076932+xdvt1337@users.noreply.github.com>
2024-09-16 13:14:31 +0700

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,30 @@
+def is_prime_number(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def is_prime(decorator_func):
+    def wrapper(*args, **kwargs):
+        result = decorator_func(*args, **kwargs)
+        if is_prime_number(result):
+            print("Простое")
+        else:
+            print("Составное")
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
